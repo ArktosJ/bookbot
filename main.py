a6a4864f17f5e62f6990d98ba5a2c7ca1fe347f9
ArktosJ <julian.soerensen@outlook.dk>
2025-07-17 21:09:17 +0200

    added the rest, this was fun

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 from stats import get_number_of_words
+from stats import chracter_counter
+from stats import sort_dict
+import sys
 
 def get_book_text(path):
     with open(path) as f:
         book_as_string = f.read()
     return book_as_string
 
+def printreport(list_with_dict):
+    for i in range(len(list_with_dict)):
+        dict_here = list_with_dict[i]
+        current_char = dict_here["char"]
+        current_number = dict_here["num"]
+        character_report = f"{current_char}: {current_number}"
+        print(character_report)
+
+def printintro():
+    print("============ BOOKBOT ============")
+
+def printoutro():
+    print("============= END ===============")
+
 def main():
-    frankenstein_path = "books/frankenstein.txt"
-    text = get_book_text(frankenstein_path)
-    number = get_number_of_words(text)
-    report_numbers = f"{number} words found in the document"
-    print(report_numbers)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        printintro()
+        path = sys.argv[1]
+        print(f"Analyzing book found at {path}")
+        text = get_book_text(path)
+        number = get_number_of_words(text)
+        print("----------- Word Count ----------")
+        report_numbers = f"Found {number} total words"
+        print(report_numbers)
+        print("--------- Character Count -------")
+        report_characters = chracter_counter(text)
+        sorted = sort_dict(report_characters)
+        printreport(sorted)
+        printoutro()
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,31 @@
 def get_number_of_words(text):
     return len(text.split())
 
-def chracter_counter():
+def chracter_counter(text):
+    text_lower = text.lower()
+    dictionary_of_characters = {}
+    for word in text_lower:
+        for character in word:
+            if character in dictionary_of_characters:
+                dictionary_of_characters[character] += 1
+            else: 
+                dictionary_of_characters[character] = 1
+    return dictionary_of_characters
+
+def sort_dict(dictionary_to_sort):
+    list_of_dict = []
+    for item in dictionary_to_sort:
+        if item.isalpha():
+            dict_to_append = {
+                "char": item,
+                "num": dictionary_to_sort[item]
+            }
+            list_of_dict.append(dict_to_append)
+            list_of_dict.sort(reverse=True, key=sort_on)
+        else:
+            continue
+    return list_of_dict
+
+def sort_on(items):
+    return items["num"]
+
